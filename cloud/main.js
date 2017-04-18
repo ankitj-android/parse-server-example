8d7b10fb7f00c30be1d25d56b45a4e2371e2e70d
Ankit Jasuja <AJasuja@walmartlabs.com>
2017-04-17 22:05:06 -0700

    added another push function

--- cloud/main.js
+++ cloud/main.js
@@ -16,9 +16,10 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
 
-  var payload = { "alert": message,
-                  "customdata": customData
-                };
+  var payload =
+  {
+    "alert": message,
+    "customdata": customData;
   };
 
 
@@ -27,6 +28,32 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   Parse.Push.send({
   where: pushQuery,      // for sending to a specific channel
   data: payload,
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
+
+
+Parse.Cloud.define('pingReply', function(request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("installationId", sender);
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
   }, { success: function() {
      console.log("#### PUSH OK");
   }, error: function(error) {
